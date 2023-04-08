d864e62e19e8cc9417a1f2895b1532709d0177d1
Matthias Bromisch <matthias.bromisch@gmail.com>
2023-04-08 16:27:51 +0200

    finished part2

---
+++ ViewModels/WeatherViewModel2.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ExamplesBlazorMvvmToolkit.Data;
+
+namespace ExamplesBlazorMvvmToolkit.ViewModels;
+
+internal sealed partial class WeatherViewModel2 : ObservableObject
+{
+    private readonly WeatherForecastService _weatherService;
+
+    public WeatherViewModel2(WeatherForecastService weatherService)
+    {
+        _weatherService = weatherService;
+    }
+
+    [ObservableProperty] private WeatherForecast[] _forecasts = Array.Empty<WeatherForecast>();
+
+    [RelayCommand]
+    private async Task Load()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(2)); // simulate loading
+        Forecasts = await _weatherService.GetForecastAsync(DateOnly.FromDateTime(DateTime.Now));
+    }
+}
